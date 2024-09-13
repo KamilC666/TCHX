7a3d7fa03e82644fbe678c15bae43f46f8586f45
Kamil <106310287+KamilC666@users.noreply.github.com>
2024-09-13 22:28:46 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,49 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const navToggle = document.getElementById('nav-toggle');
-    const navMenu = document.querySelector('.nav-menu');
-    const header = document.querySelector('header');
+// Pobranie elementów HTML
+const menuIcon = document.getElementById('menu-icon');
+const navLinks = document.getElementById('nav-links');
 
-    function adjustNavigation() {
-        const containerWidth = header.querySelector('.container').offsetWidth;
-        const logoWidth = header.querySelector('.logo-link').offsetWidth;
-        const navItems = navMenu.querySelectorAll('li');
-        let totalNavWidth = 0;
-
-        navItems.forEach(item => {
-            totalNavWidth += item.offsetWidth;
-        });
-
-        if (containerWidth - logoWidth < totalNavWidth + 20) {
-            navToggle.style.display = 'block';
-            navMenu.style.display = 'none';
-        } else {
-            navToggle.style.display = 'none';
-            navMenu.style.display = 'flex';
-        }
-    }
-
-    navToggle.addEventListener('change', function() {
-        if (this.checked) {
-            navMenu.style.display = 'flex';
-        } else {
-            navMenu.style.display = 'none';
-        }
-    });
-
-    // Close menu when a link is clicked
-    const navLinks = document.querySelectorAll('.nav-menu a');
-    navLinks.forEach(link => {
-        link.addEventListener('click', () => {
-            navToggle.checked = false;
-            if (window.innerWidth <= 768) {
-                navMenu.style.display = 'none';
-            }
-        });
-    });
-
-    //window resize
-    window.addEventListener('resize', adjustNavigation);
-
-    // Initial adjustment
-    adjustNavigation();
+// Dodanie nasłuchiwania kliknięcia na ikonę menu
+menuIcon.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
 });
